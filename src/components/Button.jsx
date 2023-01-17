f103f4ef147713b6ccc7ce8eaa5c499511089d8c
Daniel Morales <daniel.morales@pruebat.org>
2023-01-17 13:07:28 -0600

    my first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const Button = ({accion, fn}) => {
+
+    return (
+        <button
+        type="button"
+        className="h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500
+        rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 "
+        onClick={fn}
+        >{accion == "i" ? "-" : "+"}</button>
+    )
+}
+
+export default Button
